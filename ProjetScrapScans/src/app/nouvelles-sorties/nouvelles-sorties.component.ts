26589dd4ca9172cbb48dcbc172419ec3bff63967
Remi Jacques <remi.jacques12@gmail.com>
2019-11-04 21:33:52 +0100

    Added Lib sys + New component nouvelles-sorties

---
+++ ProjetScrapScans/src/app/nouvelles-sorties/nouvelles-sorties.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nouvelles-sorties',
+  templateUrl: './nouvelles-sorties.component.html',
+  styleUrls: ['./nouvelles-sorties.component.scss']
+})
+export class NouvellesSortiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- backEnd/surveillerSorties.js
+++ backEnd/surveillerSorties.js
@@ -1,58 +1,95 @@
 const axios = require('axios');
+const dlTools = require('./telechargerChapitre.js');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const request = require('request');
 
+const SITE_URL = "https://www.lelscan-vf.com/manga/";
 
 module.exports = {
     //Depuis la page d'accueil de lelscan, recupere tous les scans sortis recemment
-    recupDerniersChapitresSortis : async function (){
+    recupDerniersChapitresSortis: async function () {
         var test;
         var scansSortis = [];
-        try{
+        try {
             test = await axios.get('https://www.lelscan-vf.com/')
-                .then((reponse) =>{
+                .then((reponse) => {
                     return reponse.data;
                 })
-                .catch((error) =>{
+                .catch((error) => {
                     console.log(error);
                 });
-            
+
             const $ = cheerio.load(test);
-            $('div[class=mangalist]').find('div[class=manga-item]').each(function(index,element){
+            $('div[class=mangalist]').find('div[class=manga-item]').each(function (index, element) {
                 scansSortis[index] = {
-                    titre : $(element).find('h3 > a').text(),
-                    chapitres : []
+                    titre: $(element).find('h3 > a').text(),
+                    chapitres: []
                 };
-                
-                $(element).find('div > h6 a').each(function(index2,elem){
+
+                $(element).find('div > h6 a').each(function (index2, elem) {
                     scansSortis[index].chapitres[index2] = {
-                        titreChap : $(elem).text(),
-                        lienChap : $(elem).attr('href')
+                        titreChap: $(elem).text(),
+                        lienChap: $(elem).attr('href')
                     };
                 });
             });
-            for(var i = 0; i < scansSortis.length;i++){
+            for (var i = 0; i < scansSortis.length; i++) {
                 printManga(scansSortis[i]);
             }
             return scansSortis;
-        }
-        catch(error){
+        } catch (error) {
             console.log(error);
         }
+    },
+
+    recupDerniersChapitresSortisv2: async function () {
+        let mangasAVerifier = [];
+        let mangaStr;
+        let resJSON;
+
+        console.log("Searching for new scans..........");
+
+        mangasAVerifier = await JSON.parse(fs.readFileSync('mangas.json'));
+
+        for (let i = 0; i < mangasAVerifier.length; i++) {
+            var name = mangasAVerifier[i].name.replace(/ /gi, '-').toLowerCase();
+            mangaStr = SITE_URL + name + '/' + mangasAVerifier[i].nextChapter + '/' + 1;
+            console.log(mangaStr);
+            await axios.get(mangaStr)
+                .then((reponse) => {
+                    dlTools.telechargerUnScan(name, mangasAVerifier[i].nextChapter);
+
+                    mangasAVerifier[i].nextChapter++;
+                    console.log("Nouveau Scan de " + mangasAVerifier[i].name);
+                    //Notifier utilisateur de la sortie du scan
+
+                })
+                .catch((error) => {
+                    console.log("Pas de Nouveau Scan de " + mangasAVerifier[i].name + "\n" + error);
+                });
+        }
+        resJSON = JSON.stringify(mangasAVerifier);
+        fs.writeFile('mangas.json', resJSON, 'utf8', function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+        });
+
     }
 };
 
-function printChapitre(chap){
-    console.log("---- "+chap.titreChap);
-    console.log("---- "+chap.lienChap);
+
+
+function printChapitre(chap) {
+    console.log("---- " + chap.titreChap);
+    console.log("---- " + chap.lienChap);
 }
 
-function printManga(manga){
-    console.log("\n"+manga.titre);
-    for(var i = 0; i < manga.chapitres.length; i++){
+function printManga(manga) {
+    console.log("\n" + manga.titre);
+    for (var i = 0; i < manga.chapitres.length; i++) {
         printChapitre(manga.chapitres[i]);
     }
     console.log("\n");
 }
-
