9aa739e5942dd03b17dd979930ff9dec643c8db9
Remi Jacques <remi.jacques12@gmail.com>
2019-11-05 00:23:07 +0100

    added TODO.txt

--- ProjetScrapScans/src/app/app-routing.module.ts
+++ ProjetScrapScans/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageLecteurComponent} from './page-lecteur/page-lecteur.component';
+import { NouvellesSortiesComponent} from './nouvelles-sorties/nouvelles-sorties.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'lecteur',component : PageLecteurComponent },
+  {path :'dernieresSorties',component : NouvellesSortiesComponent },
+  {path :'',component : NouvellesSortiesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ProjetScrapScans/src/app/nouvelles-sorties/nouvelles-sorties.component.ts
+++ ProjetScrapScans/src/app/nouvelles-sorties/nouvelles-sorties.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-nouvelles-sorties',
   templateUrl: './nouvelles-sorties.component.html',
   styleUrls: ['./nouvelles-sorties.component.scss']
 })
+@Injectable()
 export class NouvellesSortiesComponent implements OnInit {
-
-  constructor() { }
+  toutesLesSorties : any[] = [];
+  constructor(private httpClient : HttpClient) { }
 
   ngOnInit() {
+    this.httpClient.get<any[]>("http://localhost:8080/recupDerniereSorties").subscribe( 
+      (reponse)=> {
+        this.toutesLesSorties = reponse;
+      })
   }
+  
 
 }
